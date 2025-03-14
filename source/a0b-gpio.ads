661fc4919642f682de81646ab4cb886f010092a8
Vadim Godunko <vgodunko@gmail.com>
2025-03-14 09:16:18 +0400

    Remove `No_Elaboration_Code` restriction...
    ... for compatibility with `embedded` GNAT runtime.

--- source/a0b-gpio.ads
+++ source/a0b-gpio.ads
@@ -1,10 +1,8 @@
 --
---  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--  Copyright (C) 2024-2025, Vadim Godunko <vgodunko@gmail.com>
 --
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
-
-pragma Restrictions (No_Elaboration_Code);
 
 package A0B.GPIO
   with Preelaborate
